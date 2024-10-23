e3b666d3d9f46cc32b3ec3c859c7315ed6ec499b
Neha Shanbhag <63840903+nshanbhag20001412@users.noreply.github.com>
2024-10-23 13:02:48 +0530

    chore:updated utils.py to restrict sparkdeafults.conf usage

--- lib/Utils.py
+++ lib/Utils.py
@@ -4,8 +4,9 @@ from pyspark.sql import SparkSession
 def get_spark_session(env):
     if env == "LOCAL":
         return SparkSession.builder \
-            .config('spark.driver.extraJavaOptions',
-                    '-Dlog4j.configuration=file:log4j.properties') \
+            .config('spark.driver.extraJavaOptions', '-Dlog4j.configuration=file:./log4j.properties') \
+            .config('spark.eventLog.enabled', 'true') \
+            .config('spark.eventLog.dir', 'file:///D:/Neha/learnspark/SBDL') \
             .master("local[2]") \
             .enableHiveSupport() \
             .getOrCreate()
